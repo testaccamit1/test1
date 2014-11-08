e5e831e08fc7482f541b4f76785523c050bd5cc0
Amit <testaccamit1@gmail.com>
2014-11-08 23:25:26 +0530

    initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+
+gcc -o hello_$BUILD_NUMBER hello.c
+
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World from jenkins\n");
+	return 0;
+}
+
